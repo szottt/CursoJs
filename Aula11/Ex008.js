10182b8aaf5447fcac9431d3abe822c8b3e3fd61
szottt <szottt@hotmail.com>
2019-09-05 10:04:09 -0300

    Aulas 11

---
+++ Aula11/Ex008.js
@@ -0,0 +1,9 @@
+var vel = 78
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+
+if (vel > 60){
+        console.log(`Voce ultrapassou a velocidade permitida, MULTADO!`)
+        }
+        
+console.log(`Diriga com segurança`)
---
+++ Aula11/Ex009.js
@@ -0,0 +1,7 @@
+var state = 'USA'
+if (state == 'BR'){
+    console.log('Voce e Brasileiro')
+}
+else{
+    console.log(`Voce é Estrangeiro e mora nos ${state}`)
+}
