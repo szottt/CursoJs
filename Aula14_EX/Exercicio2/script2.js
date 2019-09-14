876a65dc0fbc307737ba1bf6b4df0af91af34cf7
szottt <szottt@hotmail.com>
2019-09-14 11:39:06 -0300

    Exercicio numero 2 -Tabuada

---
+++ Aula14_EX/Exercicio2/script2.js
@@ -0,0 +1,25 @@
+function Tabuada(){
+
+    let num = document.getElementById('txt1')
+    let tab = document.getElementById('seltab')
+
+    if(num.value.length == 0){
+        window.alert('Por favor digite um numero')
+        
+    }else{
+        let n = Number(num.value)
+        let c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            let item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}`
+            tab.appendChild(item)
+            c++
+        }
+    }
+
+
+
+
+}
