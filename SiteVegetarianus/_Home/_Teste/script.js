4f552325e3596d36bb78a2e84293e506c387a0d2
szottt <szottt@hotmail.com>
2019-10-07 21:03:37 -0300

    Projeto Vegetarianus

---
+++ SiteVegetarianus/_Home/_Teste/script.js
@@ -0,0 +1,8 @@
+function Cadastro(){
+    let data = new Date()
+    let ano = data.getFullYear()
+    let anocad = window.document.querySelector('input#datacad')
+    let anocad2 = 
+    
+    window.alert(anocad)
+}
